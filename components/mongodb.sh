72af8d1455beabe8f8a8b044dd0e7816493c4f54
phani <phanendrasai41@gmail.com>
2022-06-20 16:41:53 +0530

    updating ./robo-shop-script

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -1,1 +1,23 @@
-echo Install mongodb component
+#!/bin/bash/
+
+
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+yum install -y mongodb-org
+systemctl enable mongod
+systemctl start mongod
+
+#Update Listen IP address from 127.0.0.1 to 0.0.0.0 in config file
+#
+#Config file: `/etc/mongod.conf`
+#
+
+systemctl restart mongod
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+
+cd /tmp
+unzip mongodb.zip
+cd mongodb-main
+mongo < catalogue.js
+mongo < users.js
+
+
