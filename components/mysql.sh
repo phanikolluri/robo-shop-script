6a280946978e2b6552b140ae683332329a5e7be6
phani <phanendrasai41@gmail.com>
2022-06-21 18:37:20 +0530

    updating ./robo-shop-script

--- components/mysql.sh
+++ components/mysql.sh
@@ -3,7 +3,7 @@ yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl start mysqld
 
-#MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
 #echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}"
 #echo "uninstall plugin validate_password;" | mysql -uroot -p"${MYSQL_PASSWORD}"
