147dd809a4ae6928468dec6f369641aa60a81e56
phani <phanendrasai41@gmail.com>
2022-06-21 08:07:09 +0530

    updating ./robo-shop-script

--- components/user.sh
+++ components/user.sh
@@ -16,5 +16,6 @@ npm install
 sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/'  -e 's/MONGO_ENDPOINT/mongo.roboshop.internal/' /home/roboshop/user/systemd.service
 mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
 systemctl daemon-reload
-systemctl restart user
+systemctl start user
 systemctl enable user
+
