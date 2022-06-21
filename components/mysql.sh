05efb79d53e1da205edffe63c59da4bf7f05a6cf
phani <phanendrasai41@gmail.com>
2022-06-21 11:56:30 +0530

    updating ./robo-shop-script

--- components/mysql.sh
+++ components/mysql.sh
@@ -8,35 +8,9 @@ MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{p
 echo "ALTER USER 'root@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}"
 echo "uninstall plugin validate_password;" | mysql -uroot -p"${MYSQL_PASSWORD}"
 
-#grep temp /var/log/mysqld.log
-
-
-#Use password `RoboShop@1` or any other as per your choice. Rest of the options you can choose `No`
-
-
-#mysql_secure_installation
-#mysql -uroot -pRoboShop@1
-
-
-#Once after login to MySQL prompt then run this SQL Command.
-
-#```sql
-#> uninstall plugin validate_password;
-
-## **Setup Needed for Application.**
-
-#As per the architecture diagram, MySQL is needed by
-
-#- Shipping Service
-
-#So we need to load that schema into the database, So those applications will detect them and run accordingly.
-
-#To download schema, Use the following command
-
-
-#curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
-#cd /tmp
-#unzip mysql.zip
-#cd mysql-main
-#mysql -uroot -pRoboShop@1 < shipping.sql
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+cd /tmp
+unzip -o mysql.zip
+cd mysql-main
+mysql -uroot -pRoboShop@1 < shipping.sql
 
