fcd59f2b7e7f82d4c62795abd6d83c84be1f2855
phani <phanendrasai41@gmail.com>
2022-06-21 12:28:41 +0530

    updating ./robo-shop-script

--- components/shipping.sh
+++ components/shipping.sh
@@ -1,1 +1,18 @@
-echo Install shipping component
+yum install maven -y
+useradd roboshop
+cd /home/roboshop
+rm -rf shipping
+curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip"
+unzip /tmp/shipping.zip
+mv shipping-main shipping
+cd shipping
+mvn clean package
+mv target/shipping-1.0.jar shipping.jar
+sed -i -e 's/CARTENNPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' /home/roboshop/shipping/systemd.service
+mv /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
+systemctl daemon-reload
+systemctl start shipping
+systemctl enable shipping
+
+
+
