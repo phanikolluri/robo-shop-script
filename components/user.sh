e0669c88ce3f95e59342eacb7f9b65aaca808e83
phani <phanendrasai41@gmail.com>
2022-06-21 07:51:12 +0530

    updating ./robo-shop-script

--- components/user.sh
+++ components/user.sh
@@ -17,5 +17,5 @@ sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongo
 
 mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
 systemctl daemon-reload
-systemctl start user
+systemctl restart user
 systemctl enable user
