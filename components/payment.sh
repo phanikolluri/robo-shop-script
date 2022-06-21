bdbf4f120df9239be8b4368e3e05cf9751d23971
phani <phanendrasai41@gmail.com>
2022-06-21 13:25:52 +0530

    updating ./robo-shop-script

--- components/payment.sh
+++ components/payment.sh
@@ -1,1 +1,16 @@
-echo Install payment component
+yum install python36 gcc python3-devel -y
+useradd roboshop
+cd /home/roboshop
+rm -rf payment
+curl -L -s -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip"
+unzip /tmp/payment.zip
+mv payment-main payment
+cd /home/roboshop/payment
+pip3 install -r requirements.txt
+
+#sed -i -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' /home/roboshop/payment/systemd.service
+#mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service
+#systemctl daemon-reload
+#systemctl enable payment
+#systemctl start payment
+
