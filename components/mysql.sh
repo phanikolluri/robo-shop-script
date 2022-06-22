8f7e80d120ca598d54dda0800cb9d5b2bdd8efce
phani <phanendrasai41@gmail.com>
2022-06-22 08:13:21 +0530

    updating ./robo-shop-script

--- components/mysql.sh
+++ components/mysql.sh
@@ -11,6 +11,6 @@ curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/a
 cd /tmp
 unzip -o mysql.zip
 cd mysql-main
-mysql -uroot -pRoboShop@1 <shipping.sql
+mysql -u root -p"${MYSQL_PASSWORD}" <shipping.sql
 
 
