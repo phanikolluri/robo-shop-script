40d46d599f8a865837cae240cd9dfcdb96519d7a
phani <phanendrasai41@gmail.com>
2022-06-22 05:55:41 +0530

    updating ./robo-shop-script

--- components/mysql.sh
+++ components/mysql.sh
@@ -1,17 +1,17 @@
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-yum install mysql-community-server -y
-systemctl enable mysqld
-systemctl start mysqld
+#curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+#yum install mysql-community-server -y
+#systemctl enable mysqld
+#systemctl start mysqld
 
-MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+#MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}"
-echo "uninstall plugin validate_password;" | mysql -uroot -p"${MYSQL_PASSWORD}"
+#echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}"
+#echo "uninstall plugin validate_password;" | mysql -uroot -p"${MYSQL_PASSWORD}"
 
-curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
-cd /tmp
-unzip -o mysql.zip
-cd mysql-main
-mysql -uroot -pRoboShop@1 <shipping.sql
+#curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+#cd /tmp
+#unzip -o mysql.zip
+#cd mysql-main
+#mysql -uroot -pRoboShop@1 <shipping.sql
 
 
--- components/rabbitmq.sh
+++ components/rabbitmq.sh
@@ -1,21 +1,13 @@
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
-yum install rabbitmq-server -y
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_user_tags roboshop administrator
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
-
-REDIS
-
-curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
-yum install redis-6.2.7 -y
-
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+#yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y
+#curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+#yum install rabbitmq-server -y
+#systemctl enable rabbitmq-server
+#systemctl start rabbitmq-server
+#rabbitmqctl add_user roboshop roboshop123
+#rabbitmqctl set_user_tags roboshop administrator
+#rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
 
 
-systemctl enable redis
-systemctl restart redis
+
 
 
