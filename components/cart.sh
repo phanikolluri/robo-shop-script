fc4a131135cd5ce316254716b509b918f39e7f31
phani <phanendrasai41@gmail.com>
2022-06-23 16:40:00 +0530

    updating ./robo-shop-script

--- components/cart.sh
+++ components/cart.sh
@@ -28,14 +28,14 @@ rm -rf cart &>>${LOG}
 CHECK_STAT $?
 
 PRINT "Extract CART content"
-unzip /tmp/cart.zip &>>${LOG}
+unzip  /tmp/cart.zip &>>${LOG}
 CHECK_STAT $?
 
 PRINT cart-main cart
 cd cart
 
 PRINT "Install NODEJS dependencies for cart component"
-npm install &>>${LOG}
+npm  install &>>${LOG}
 CHECK_STAT $?
 
 PRINT "Update Systemd Configuration"
