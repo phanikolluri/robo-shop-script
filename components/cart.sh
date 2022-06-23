343c888bf61cfdfa48191c21964c6bebb79d3adc
phani <phanendrasai41@gmail.com>
2022-06-23 15:31:50 +0530

    updating ./robo-shop-script

--- components/cart.sh
+++ components/cart.sh
@@ -2,25 +2,53 @@ source components/common.sh
 
 CHECK_ROOT
 
-echo "Setting Up NODEJS Yum Repo is "
+echo "Setting Up NODEJS Yum Repo"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
 CHECK_STAT $?
 
-yum install nodejs -y
-useradd roboshop
-curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+echo "Installing NODEJS"
+yum install nodejs -y &>>${LOG}
+CHECK_STAT $?
+
+echo "Creating application User"
+useradd roboshop &>>${LOG}
+CHECK_STAT $?
+
+echo "Downloading CART content"
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>${LOG}
+CHECK_STAT $?
+
 cd /home/roboshop
-rm -rf cart
-unzip /tmp/cart.zip
+
+echo "Remove old content"
+rm -rf cart &>>${LOG}
+CHECK_STAT $?
+
+echo "Extract CART content"
+unzip /tmp/cart.zip &>>${LOG}
+CHECK_STAT $?
+
 mv cart-main cart
 cd cart
-npm install
 
-sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/cart/systemd.service
-mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+echo "Install NODEJS dependencies for cart component"
+npm install &>>${LOG}
+CHECK_STAT $?
+
+echo "Update Systemd Configuration"
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/cart/systemd.service &>>${LOG}
+CHECK_STAT $?
+
+echo "Setup Systemd configuration"
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>${LOG}
+CHECK_STAT $?
+
 systemctl daemon-reload
 systemctl restart cart
-systemctl enable cart
+
+echo "start CART service"
+systemctl enable cart &>>${LOG}
+CHECK_STAT $?
 
 
 
