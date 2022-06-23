5dfcb291937ecc46b86c14841e0f60a1626038b5
phani <phanendrasai41@gmail.com>
2022-06-23 10:46:34 +0530

    updating ./robo-shop-script

--- components/payment.sh
+++ components/payment.sh
@@ -1,3 +1,12 @@
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]; then
+    echo You are Non root user
+    echo You can run this script as root user or with sudo
+    exit1
+fi
+
+
+
 yum install python36 gcc python3-devel -y
 useradd roboshop
 cd /home/roboshop
