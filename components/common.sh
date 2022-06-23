7a9e6729e05938cb6026e264a4fe77b54fee93f4
phani <phanendrasai41@gmail.com>
2022-06-23 12:45:01 +0530

    updating ./robo-shop-script

--- components/common.sh
+++ components/common.sh
@@ -1,8 +1,7 @@
 CHECK_ROOT() {
  USER_ID=$(id -u)
  if [ $USER_ID -ne 0 ]; then
-     echo You are Non root user
-     echo You can run this script as root user or with sudo
+     echo -e "\e[31m You can run this script as root user or sudo with script\e[0m"
      exit 1
  fi
 }
