982dff935b2eefb61a6930e15aaf9693ed72b8c7
phani <phanendrasai41@gmail.com>
2022-06-24 12:26:25 +0530

    updating ./robo-shop-script

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -2,21 +2,29 @@ source components/common.sh
 
 CHECK_ROOT
 
+PRINT "Configure yum repos"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG}
+CHECK_STAT $?
 
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
-yum install -y mongodb-org
-systemctl enable mongod
-systemctl start mongod
+PRINT "Install mongodb"
+yum install -y mongodb-org &>>${LOG}
+CHECK_STAT $?
 
+PRINT "Configure mongodb service"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-systemctl restart mongod
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+CHECK_STAT $?
 
-cd /tmp
-unzip -o mongodb.zip
-cd mongodb-main
-mongo < catalogue.js
-mongo < users.js
+PRINT "Start mongodb"
+systemctl enable mongod &>>${LOG} && systemctl restart mongod &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Download schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>${LOG}
+CHECK_STAT $?
+
+PRINT "load schema"
+cd /tmp && unzip -o mongodb.zip &>>${LOG} && cd mongodb-main && mongo < catalogue.js &>>${LOG} && mongo < users.js &>>${LOG}
+CHECK_STAT $?
 
 
 
