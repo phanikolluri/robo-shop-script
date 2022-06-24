e161a53ca8b16d7760f36bd29b485d42c47f5a30
phani <phanendrasai41@gmail.com>
2022-06-24 12:55:44 +0530

    updating ./robo-shop-script

--- components/mysql.sh
+++ components/mysql.sh
@@ -2,6 +2,10 @@ source components/common.sh
 
 CHECK_ROOT
 
+if [ -z "${MYSQL_PASSWORD}" ]; then
+  echo "Need MYSQL_PASSWORD env variable"
+  exit
+fi   
 
 PRINT "Configure yum repos"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG}
@@ -21,7 +25,6 @@ echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql 
 CHECK_STAT $?
 
 exit
-
 echo "uninstall plugin validate_password;" | mysql -uroot -p"${MYSQL_PASSWORD}"
 
 curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
