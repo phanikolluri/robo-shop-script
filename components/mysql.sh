2a80847997bcb7079474e4cc7eb26e7e1623ab6f
phani <phanendrasai41@gmail.com>
2022-06-24 18:33:26 +0530

    updating ./robo-shop-script

--- components/mysql.sh
+++ components/mysql.sh
@@ -37,6 +37,7 @@ PRINT "Download schema"
 curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>${LOG}
 CHECK_STAT $?
 
+PRINT "Load schema"
 cd /tmp && unzip -o mysql.zip &>>${LOG} && cd mysql-main && mysql -u root -p"${MYSQL_PASSWORD}" <shipping.sql &>>${LOG}
 CHECK_STAT $?
 
