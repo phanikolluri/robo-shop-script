d57f7116e58553e1ac239285dbf506bee6a3ee23
phani <phanendrasai41@gmail.com>
2022-06-25 11:30:25 +0530

    updating ./robo-shop-script

---
+++ components/rabbitmq.sh
@@ -0,0 +1,28 @@
+source components/common.sh
+
+CHECK_ROOT
+
+if [ -z "$RABBITMQ_USER_PASSWORD" ]; then
+  echo "Env variable RABBITMQ_USER_PASSWORD needed"
+  exit 1
+fi
+
+PRINT "setup yum repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Install Erlang & Rabbitmq"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm  rabbitmq-server -y &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Start Rabbitmq service"
+systemctl enable rabbitmq-server &>>${LOG} && systemctl start rabbitmq-server &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Create RABBITMQ user"
+  rabbitmqctl add_user roboshop ${RABBITMQ_USER_PASSWORD} &>>${LOG}
+  CHECK_STAT $?
+
+PRINT "RABBITMQ user Tags and Permissions"
+rabbitmqctl set_user_tags roboshop administrator &>>${LOG} && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${LOG}
+CHECK_STAT $?
