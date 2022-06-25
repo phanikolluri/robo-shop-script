238d51e20564bb004b3be00ec628e56c43dcc0b7
phani <phanendrasai41@gmail.com>
2022-06-25 08:40:20 +0530

    updating ./robo-shop-script

--- components/mysql.sh
+++ components/mysql.sh
@@ -26,7 +26,7 @@ if [ $? -ne 0 ]; then
 fi
 
 exit 
-echo "uninstall plugin validate_password;" | mysql -uroot -p"${MYSQL_PASSWORD}" &>>${LOG}
+echo "uninstall plugin validate_password;" | mysql -uroot -p"${MYSQL_PASSWORD}" 
 
 curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" 
 cd /tmp
