cba99c215bee7e6806636d79bad6dca6575f715a
phani <phanendrasai41@gmail.com>
2022-06-25 10:40:43 +0530

    updating ./robo-shop-script

--- components/rabbitmq.sh
+++ components/rabbitmq.sh
@@ -2,15 +2,30 @@ source components/common.sh
 
 CHECK_ROOT
 
+if [ -z "$RABBITMQ_USER_PASSWORD" ]; then
+  echo "Env variable RABBITMQ_USER_PASSWORD needed"
+  exit 1
+fi
 
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
-yum install rabbitmq-server -y
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_user_tags roboshop administrator
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+PRINT "setup yum repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Install Erlang & Rabbitmq"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm  rabbitmq-server -y &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Start Rabbitmq service"
+systemctl enable rabbitmq-server &>>${LOG} && systemctl start rabbitmq-server &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Create RABBITMQ user"
+rabbitmqctl add_user roboshop roboshop123 &>>${LOG}
+CHECK_STAT $?
+
+PRINT "RABBITMQ user Tags and Permissions"
+rabbitmqctl set_user_tags roboshop administrator &>>${LOG} && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${LOG}
+CHECK_STAT $?
 
 
 
